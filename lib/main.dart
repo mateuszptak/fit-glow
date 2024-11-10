95ea22d7c258998026a7799e0a540290b50309dd
Mateusz Ptak <mateusz.ptak2612@gmail.com>
2024-11-10 18:04:36 +0100

    add example bottom navigation bar with three items

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/pages/bmi_calculator.dart';
 import 'package:flutter_application_2/pages/welcome_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const WelcomePage(),
+      home: const BmiCalculator(),
     );
   }
 }
--- lib/pages/bmi_calculator.dart
+++ lib/pages/bmi_calculator.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/widgets/custom_background_image.dart';
 
 class BmiCalculator extends StatelessWidget {
   const BmiCalculator({super.key});
@@ -8,19 +9,41 @@ class BmiCalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                  Navigator.of(context).pop();
-                },
-                child: const Text("LogOut();"),
+        extendBody: true,
+        body: CustomBackgroundImage(
+          backgroundImageUrl:
+              "https://4kwallpapers.com/images/wallpapers/dark-background-abstract-background-network-3d-background-2048x2048-8324.png",
+          child: Column(
+            children: [
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("LogOut();"),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            items: const [
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(Icons.scale, color: Colors.white),
+              ),
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(Icons.line_weight_outlined, color: Colors.white),
+              ),
+              BottomNavigationBarItem(
+                label: "",
+                icon: Icon(Icons.online_prediction_sharp, color: Colors.white),
+              ),
+            ]),
       ),
     );
   }
